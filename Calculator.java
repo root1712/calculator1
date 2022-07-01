cd47fe3819d321aa166a24da7a35339fad20ddce
Shruti Patil <shrutimpatil@gmail.com>
2022-07-01 09:54:52 +0530

    all calculations

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+
+	import java.util.Scanner;
+
+	public class Calculator {
+		
+        public static void main(String[] args){
+        double num1 = 10;
+        double num2 = 20;
+        double res;
+
+        res=num1+num2;
+        System.out.println("Summation: "+res);
+        res=num1-num2;
+        System.out.println("Subtraction: "+res);
+        res=num1*num2;
+        System.out.println("Multiplication: "+res);
+        res=num1/num2;
+        System.out.println("Division: "+res);
+    }
+
+
+}
